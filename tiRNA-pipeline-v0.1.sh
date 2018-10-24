eb81e4aa9f8108a344171528ca90e07e52ca7c48
Paul Donovan <pauldonovandonegal@gmail.com>
2018-10-24 16:37:32 +0100

    Added Trim_Galore

--- tiRNA-pipeline-v0.1.sh
+++ tiRNA-pipeline-v0.1.sh
@@ -6,4 +6,8 @@
 # 19-Oct-2018
 
 echo "Started at $(date)"
-StartTime="Pipeline initiated at $(date)" 
+StartTime="Pipeline initiated at $(date)"
+
+
+
+trim_galore -o trim_galore_output/ --paired Reads/SRR5788497_1_10000-reads.fq Reads/SRR5788497_2_10000-reads.fq
