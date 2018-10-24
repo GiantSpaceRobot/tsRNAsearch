b04520b8587be5da4b9b1936bbdb9a3d9f61d05a
Paul Donovan <pauldonovandonegal@gmail.com>
2018-10-24 16:44:59 +0100

    Created initial getopts parameter structure for pipeline

--- tiRNA-pipeline-v0.1.sh
+++ tiRNA-pipeline-v0.1.sh
@@ -5,9 +5,29 @@
 # Email: pauldonovan@rcsi.com
 # 19-Oct-2018
 
-echo "Started at $(date)"
-StartTime="Pipeline initiated at $(date)"
+usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
 
+while getopts ":s:p:" o; do
+    case "${o}" in
+        s)
+            s=${OPTARG}
+            ((s == 45 || s == 90)) || usage
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
+if [ -z "${s}" ] || [ -z "${p}" ]; then
+    usage
+fi
 
-trim_galore -o trim_galore_output/ --paired Reads/SRR5788497_1_10000-reads.fq Reads/SRR5788497_2_10000-reads.fq
+#echo "Started at $(date)"
+#StartTime="Pipeline initiated at $(date)"
+
+#trim_galore -o trim_galore_output/ --paired Reads/SRR5788497_1_10000-reads.fq Reads/SRR5788497_2_10000-reads.fq
