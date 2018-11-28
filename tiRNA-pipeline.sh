a8d79b1391499b7dc6a1c5b4467160b39f225392
Paul Donovan <pauldonovandonegal@gmail.com>
2018-11-28 14:16:38 +0000

    Updated checkpoint statements and general commenting
    Former-commit-id: 4747ca85a490a75c5668f6f19d583d60e54eaa6b

--- tiRNA-pipeline.sh
+++ tiRNA-pipeline.sh
@@ -181,6 +181,7 @@ elif [[ $pairedEnd = "False" ]]; then
 	# Align trimmed reads to tRNA database using HISAT2/Tophat2
 	if [[ $oldAligner = "yes" ]]; then
 		if [ ! -f $outDir/tRNA-alignment/align_summary.txt ]; then
+			string_padder "Running tRNA alignment step..."
 			tophat2 -p $CPUs -x 1 -o $outDir/tRNA-alignment/ DBs/bowtie2_index/hg19-wholetRNA-CCA $outDir/trim_galore_output/$trimmedFile
 			bedtools bamtofastq -i $outDir/tRNA-alignment/unmapped.bam -fq $outDir/tRNA-alignment/unmapped.fastq	
 		else
@@ -191,10 +192,8 @@ elif [[ $pairedEnd = "False" ]]; then
 		#tophat2 -p $CPUs -x 1 -o $outDir/tRNA-alignment/ DBs/bowtie2_index/hg38-tRNAs_CCA $outDir/trim_galore_output/$trimmedFile
 		
 		# tophat2 has a bug that causes it to crash if none of the reads map (I think). If it crashes (and no results are generated), use the unmapped.fastq from the tRNA alignment step
-		if [ ! -f $outDir/snomiRNA-alignment/unmapped.fastq ]; then # If this file was not generated, try and align the unmapped reads from the tRNA laignment	
-			echo "
-			sno/miRNA alignment output not found. Running...
-			"
+		if [ ! -f $outDir/snomiRNA-alignment/unmapped.fastq ]; then # If this file was not generated, try and align the unmapped reads from the tRNA alignment	
+			string_padder "Runnng sno/miRNA alignment step..."
 			tophat2 -p $CPUs -x 1 -o $outDir/snomiRNA-alignment/ DBs/bowtie2_index/hg19-snomiRNA $outDir/tRNA-alignment/unmapped.fastq		
 		fi
 		if [ ! -f $outDir/snomiRNA-alignment/align_summary.txt ]; then # If this file was still not generated, use the unmapped FASTQ from the first alignment output
@@ -207,11 +206,11 @@ elif [[ $pairedEnd = "False" ]]; then
 			bedtools bamtofastq -i $outDir/snomiRNA-alignment/unmapped.bam -fq $outDir/snomiRNA-alignment/unmapped.fastq
 			unmappedReads="$outDir/snomiRNA-alignment/unmapped.fastq"
 		fi
-		
+		if [ ! -f $outDir/mRNA-ncRNA-alignment/unmapped.fastq ]; then # If this file was not generated, try and align the unmapped reads from the tRNA alignment	
+			string_padder "Running mRNA/ncRNA alignment step..."
+			tophat2 -p $CPUs -o $outDir/mRNA-ncRNA-alignment/ DBs/bowtie2_index/Homo_sapiens.GRCh37.dna.primary_assembly $unmappedReads		
+		fi		
 
-		# mRNA-ncRNA alignment step goes here!
-
-		#tophat2 -p $CPUs -o $outDir/ncRNA-mRNA-alignment/ DBs/bowtie2_index/Homo_sapiens.GRCh37.dna.primary_assembly.fa $unmappedReads	
 	else
 		hisat2 -p $CPUs -x DBs/hisat2_index/hg38-tRNAs_CCA -U $outDir/trim_galore_output/$trimmedFile -S $outDir/tRNA-alignment/aligned_tRNAdb.sam
 		string_padder "This version of the pipeline is not finished yet. Please use the '-T' parameter to use the Tophat2 version"
@@ -220,25 +219,43 @@ fi
 
 
 # Produce read counts for the three alignment steps. If one of the alignment steps failed, use an empty htseq-count output file.
-string_padder "Alignment steps complete. Moving on to read counting using HTSeq-count"
+string_padder "Alignment steps complete. Moving on to read-counting using HTSeq-count"
 # Count for alignment step 1
 if [ ! -f $outDir/tRNA-alignment/accepted_hits.bam ]; then
-	cp $outDir/additional-files/empty.count $outDir/HTSeq-count-output/tRNA-alignment.count
+	echo "
+No alignment file found for tRNA alignment. Using blank count file instead
+"
+	cp additional-files/empty.count $outDir/HTSeq-count-output/tRNA-alignment.count
 else
+	echo "
+Counting tRNA alignment reads
+"
 	htseq-count -f bam $outDir/tRNA-alignment/accepted_hits.bam DBs/hg19-wholetRNA-CCA.gtf > $outDir/HTSeq-count-output/tRNA-alignment.count
 fi
 
 # Count for alignment step 2
 if [ ! -f $outDir/snomiRNA-alignment/accepted_hits.bam ]; then
-	cp $outDir/additional-files/empty.count $outDir/HTSeq-count-output/snomiRNA-alignment.count
+	echo "
+No alignment file found for sno/miRNA alignment. Using blank count file instead
+"
+	cp additional-files/empty.count $outDir/HTSeq-count-output/snomiRNA-alignment.count
 else
+	echo "
+Counting sno/miRNA alignment reads
+"
 	htseq-count -f bam $outDir/snomiRNA-alignment/accepted_hits.bam DBs/hg19-snomiRNA.gtf > $outDir/HTSeq-count-output/snomiRNA-alignment.count
 fi
 
 # Count for alignment step 3
 if [ ! -f $outDir/mRNA-ncRNA-alignment/accepted_hits.bam ]; then
-	cp $outDir/additional-files/empty.count $outDir/HTSeq-count-output/mRNA-ncRNA-alignment.count
+	echo "
+No alignment file found for mRNA/ncRNA alignment. Using blank count file instead
+"
+	cp additional-files/empty.count $outDir/HTSeq-count-output/mRNA-ncRNA-alignment.count
 else
+	echo "
+Counting mRNA/ncRNA alignment reads
+"
 	htseq-count -f bam $outDir/tRNA-alignment/accepted_hits.bam DBs/hg19-mRNA-ncRNA.gtf > $outDir/HTSeq-count-output/mRNA-ncRNA-alignment.count
 fi
 
