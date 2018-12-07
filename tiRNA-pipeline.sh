0021d894e5001d882a749328f6768551e93b208b
Paul Donovan <pauldonovandonegal@gmail.com>
2018-12-07 14:58:37 +0000

    snomiRNA transcriptome assembly seems to be working now
    Former-commit-id: d300cf9039dcc9f8c814fe8327c51e5c5b1f32d1

--- tiRNA-pipeline.sh
+++ tiRNA-pipeline.sh
@@ -221,14 +221,17 @@ elif [[ $pairedEnd = "False" ]]; then
 	        string_padder "Found tRNA alignment file. Skipping this step."
 		fi
 
+		
+		### This section has problems that I can't figure out. Tophat2 will not build the transcriptome as I want it to. 
+		### My options are to skip this step completely, or use tophat2 mapping to the bowtie2 build of the FASTA sequence.
 		# tophat2 has a bug that causes it to crash if none of the reads map (I think). If it crashes (and no results are generated), use the unmapped.fastq from the tRNA alignment step
 		if [ ! -f $outDir/snomiRNA-alignment/$trimmedFile ]; then # If this file was not generated, try and align the unmapped reads from the tRNA alignment
 			if [ ! -d DBs/snomiRNA_DB ]; then
 				echo "Creating one-time sno/miRNA transcriptome"
-				tophat2 -p $CPUs -G DBs/hg19-snomiRNA.gtf --transcriptome-index=DBs/snomiRNA_DB/known DBs/bowtie2_index/hg19-snomiRNA
+				tophat2 -p $CPUs -G DBs/hg19-snomiRNA.gtf --transcriptome-index=DBs/snomiRNA_DB/known DBs/bowtie2_index/Homo_sapiens.GRCh37.dna.primary_assembly
 			fi
 			string_padder "Runnng sno/miRNA alignment step..."
-			tophat2 -p $CPUs -x 1 -T -N 1 -o $outDir/snomiRNA-alignment/ --transcriptome-index=DBs/snomiRNA_DB/known DBs/bowtie2_index/hg19-snomiRNA $outDir/tRNA-alignment/$trimmedFile		
+			tophat2 -p $CPUs -x 1 -T -N 1 -o $outDir/snomiRNA-alignment/ --transcriptome-index=DBs/snomiRNA_DB/known DBs/bowtie2_index/Homo_sapiens.GRCh37.dna.primary_assembly $outDir/tRNA-alignment/$trimmedFile		
 		fi
 		if [ ! -f $outDir/snomiRNA-alignment/align_summary.txt ]; then # If this file was still not generated, use the unmapped FASTQ from the first alignment output
 			echo "
@@ -242,6 +245,9 @@ elif [[ $pairedEnd = "False" ]]; then
 			bedtools bamtofastq -i $outDir/snomiRNA-alignment/unmapped.bam -fq $outDir/snomiRNA-alignment/$trimmedFile
 			#unmappedReads="$outDir/snomiRNA-alignment/$trimmedFile"
 		fi
+		
+		
+		
 		#touch $outDir/checkpoints/checkpoint-3.flag
 		if [ ! -f $outDir/mRNA-ncRNA-alignment/align_summary.txt ]; then # If this file was not generated, try and align the unmapped reads from the tRNA alignment	
 			if [ ! -d DBs/mRNA-ncRNA_DB ]; then
