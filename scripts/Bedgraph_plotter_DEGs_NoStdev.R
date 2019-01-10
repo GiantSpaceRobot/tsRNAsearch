7bb6a0e350425d1c66db1435e1e491d5afdb6061
Paul Donovan <pauldonovandonegal@gmail.com>
2019-01-10 16:50:16 +0000

    Added cutoff to snomiRNA plotting and other small fixes
    Former-commit-id: d7efdf62ef1c8d8da2d30424c487df4b16497b69

--- scripts/Bedgraph_plotter_DEGs_NoStdev.R
+++ scripts/Bedgraph_plotter_DEGs_NoStdev.R
@@ -11,6 +11,7 @@ args = commandArgs(trailingOnly=TRUE)
 
 ### Change this if you want only to plot features with a higher mean coverage
 ### (e.g. mean coverage of 100 reads is 'mean.cutoff <- 100')
+#mean.cutoff <- 0
 mean.cutoff <- args[4]
 
 ### Check if the correct number of command line arguments were provided. If not, return an error.
@@ -30,6 +31,7 @@ input2 <- read.table(args[2])
 
 ### Get names of conditions from arg3 name for writing in plots
 file3 <- basename(args[3])
+#file3 <- "condition1_vs_condition2_DEGs.pdf"
 conditions.for.plots <- strsplit(x = file3, "_DEGs")[[1]][1] #Must do double index to access the resulting list from strsplit
 condition1 <- strsplit(x = conditions.for.plots, "_vs_")[[1]][1]
 condition2 <- strsplit(x = conditions.for.plots, "_vs_")[[1]][2]
@@ -41,7 +43,9 @@ featuresUnion <- union(features, features) # Get unique set of features
 ### Open a PDF for writing
 plot_list = list()
 
-#pdf(args[3])
+#pdf("/home/paul/Documents/Pipelines/tirna-pipeline/Output_NoStdev.pdf")
+
+pdf(args[3])
 for(feature in featuresUnion) {
   
   ### file 1
@@ -62,27 +66,51 @@ for(feature in featuresUnion) {
   
   ### combine dataframes
   new.df <- rbind(subset1.NoFlanks, subset2.NoFlanks)
+  new.df2 <- cbind(subset1.NoFlanks, subset2.NoFlanks)
+  cols <- seq(1, length(new.df2))
+  cols <- paste0("V", cols)
+  colnames(new.df2) <- c(cols)
   
   ### Plot only if the mean is above the min (arg4)
   if(subset1.mean > mean.cutoff | subset2.mean > mean.cutoff){
-  p = ggplot(new.df, aes(x=coordinates, y=V10, fill=Conditions), show.legend = FALSE) +
-      geom_ribbon(aes(ymin=V10-V11, ymax=V10+V11, colour=Conditions),
-                  alpha=0.3) +
-      geom_line(size=0.8) +
-      xlab('Nucleotide position') +
-      ylab('Read coverage') +
-      scale_fill_manual(values=c("red", "blue")) +
-      scale_color_manual(values=c("red", "blue")) +
-      labs(fill="Conditions", title = feature)
-  plot_list[[feature]] = p
+  #p = ggplot(new.df2) + 
+  #        geom_line(aes(x=V12, y=V10),
+  #            color = "red") +
+  #        geom_line(aes(x=V12, y=V23),
+  #          color = "blue") +
+  #        xlab('Nucleotide position') +
+  #        ylab('Read coverage') +
+  #        labs(fill="Conditions", title = feature) +
+  #        annotate("text", x = 2, y=-100, label = condition1, color = "red") + 
+  #        annotate("text", x = 20, y=-100, label = condition2, color = "blue") 
+  min.y <- min(c(new.df2$V10, new.df2$V23))
+  max.y <- max(c(new.df2$V10, new.df2$V23))
+  plot(x = new.df2$V12, y = new.df2$V10, 
+       ylim = c(min.y, max.y),
+       #log = "y", 
+       type = "l", 
+       col = "red", 
+       main=feature, 
+       xlab="Nucleotide position", 
+       ylab="Read coverage") 
+  #points(x = new.df2$V12, y = new.df2$V10 , type = "l", col = 1) +
+  points(x = new.df2$V12, y = new.df2$V23, 
+         #log = "y", 
+         type = "l", 
+         col = "blue") 
+  #legend("bottomleft", legend = (c(condition1, condition2)), border = "black", bg = "white", col=c("red","blue"), pch=1)
+  legend("bottomright", legend = (c(condition1, condition2)), border = "black", bg = "white", col=c("red","blue"), pch=1)
+  #plot_list[[feature]] = p
   }
 }
-
-#pdf("/home/paul/Documents/Pipelines/tirna-pipeline/NewOutput2/Results/Output2.pdf")
-pdf(args[3])
-for(feature in featuresUnion) {
-  print(plot_list[[feature]])
-}
 dev.off()
+
+
+#pdf("/home/paul/Documents/Pipelines/tirna-pipeline/Output_NoStdev.pdf")
+#pdf(args[3])
+#for(feature in featuresUnion) {
+#  print(plot_list[[feature]])
+#}
+#dev.off()
 
 
