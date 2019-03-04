a93dcb274df04d984a2fff5b24660fe551c0f326
Paul Donovan <pauldonovandonegal@gmail.com>
2019-03-04 14:02:26 +0000

    Small updates to setup.sh
    Former-commit-id: 397245aeac3f87f5ef3441cef01acd4dc82319f7

--- setup.sh
+++ setup.sh
@@ -33,18 +33,18 @@ echo "Downloading human genome and GTF files..."
 wget -q http://ftp.ensembl.org/pub/grch37/release-87/gtf/homo_sapiens/Homo_sapiens.GRCh37.87.gtf.gz ./ &
 wget -q http://ftp.ensembl.org/pub/grch37/release-87/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.dna.primary_assembly.fa.gz ./ &
 
-#python 2.7.15rc1 #numpy #sys
+# python 2.7.15rc1 
 echo "Looking for Python..."
 if ! [ -x "$(command -v python)" ]; then
 	sudo apt install python
 else
-	#PyVersion=$(python -V)
+	#PyVersion=$(python -V) # Get python version
 	echo "Python already installed"
 fi
 
 echo "Looking for Rscript..."
 if ! [ -x "$(command -v Rscript)" ]; then
-	sudo apt install r-base  #Rscript 3.4.4 #ggplot2 #gplots
+	sudo apt install r-base  #Rscript 3.4.4 
 else
 	echo "Rscript already installed"
 fi
@@ -70,12 +70,12 @@ if ! [ -x "$(command -v fastqc)" ]; then
 fi
 if ! [ -x "$(command -v trim_galore)" ]; then # Check for global trim_galore
 	if [ ! -f scripts/trim_galore ]; then # Check for tsRNAsearch trim_galore
+		### Download trim_galore and move it to scripts Dir. Edit tsRNAsearch to point to new trim_galore location
 		curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.4.5.tar.gz -o trim_galore.tar.gz
 		tar xvzf trim_galore.tar.gz
 		mv TrimGalore-0.4.5/trim_galore scripts/
 		sed -i -e 's/trim_galore\ /scripts\/trim_galore\ /g' tsRNAsearch.sh
 	fi
-	#echo "trim_galore downloaded and uncompressed. Please add trim_galore to your path" >> Setup_instructions.txt
 else
 	echo "Trim_Galore aleady installed"
 fi
@@ -90,12 +90,12 @@ fi
 echo "Looking for featureCounts..."
 if ! [ -x "$(command -v featureCounts)" ]; then
 	if [ ! -f scripts/featureCounts ]; then
+		### Download featureCounts and move it to scripts Dir. Edit tsRNAsearch to point to new featureCounts location
 		wget -q https://sourceforge.net/projects/subread/files/subread-1.6.3/subread-1.6.3-Linux-x86_64.tar.gz ./ #featureCounts 1.6.3
 		tar xvfz subread-1.6.3-Linux-x86_64.tar.gz
 		mv subread-1.6.3-Linux-x86_64/bin/featureCounts scripts/
 		sed -i -e 's/featureCounts\ /scripts\/featureCounts\ /g' tsRNAsearch.sh
 	fi
-	#echo "Subreads (incl. bin/featureCounts) downloaded and uncompressed. Please add featureCounts to your path" >> Setup_instructions.txt
 else
 	echo "featureCounts already installed"
 fi
@@ -110,11 +110,4 @@ wait
 mv Homo_sapiens.GRCh37.87.gtf DBs/
 echo "Building human genome index..."
 hisat2-build -p $CPUs Homo_sapiens.GRCh37.dna.primary_assembly.fa DBs/hisat2_index/Homo_sapiens.GRCh37.dna.primary_assembly
-#echo "
-#
-#PLEASE READ Setup_instructions.txt IF THIS FILE EXISTS
-#
-#If it does not exist, you should be ready to execute the tiRNA-pipeline
-#"
-
-
+echo "Done"
