db6a2e093d0f04d7d097d6e9352715e5f071c5bf
Paul Donovan <pauldonovandonegal@gmail.com>
2019-03-04 16:05:19 +0000

    Fixed Bedgraph_plotter-DEGs-v3.R script to work with more than 3 reps
    Former-commit-id: 8618a7faf57e225e1deb004c91856efe0d3eaee2

--- scripts/Bedgraph_plotter_DEGs-v4.R
+++ scripts/Bedgraph_plotter_DEGs-v4.R
@@ -23,8 +23,8 @@ if (length(args)==0) {
 library(ggplot2)
 
 ### Read in files and get file basename
-#input1 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/Intermediate-files/condition1_concatenated_mean_stdev.snomiRNA.depth")
-#input2 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/Intermediate-files/condition2_concatenated_mean_stdev.snomiRNA.depth")
+#input1 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/Intermediate-files/condition1_concatenated_mean_stdev.tiRNA.depth")
+#input2 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/Intermediate-files/condition2_concatenated_mean_stdev.tiRNA.depth")
 #input3 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/DE_Results/DESeq2/DEGs_names-only.txt")
 #mean.cutoff = 0
 
@@ -68,6 +68,7 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
     } else next  # Skip this iteration and do not generate plot
     
     ### file 1
+    #feature <- "ValCAC"
     subset1 <- input1[grep(feature, input1$V1),]
     #subset1.NoFlanks <- tail(subset1, -10)
     #subset1.NoFlanks <- head(subset1.NoFlanks, -10)
@@ -75,6 +76,14 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
     subset1.NoFlanks$coordinates <- 1:nrow(subset1.NoFlanks)   # Make a new column with coordinates starting from 1
     subset1.NoFlanks$Conditions <- condition1
     subset1.mean <- mean(subset1.NoFlanks[,ncol(subset1.NoFlanks)-3]) # Get the mean column at the end of the dataframe
+    subset1.meancol.num <- ncol(subset1.NoFlanks)-3
+    subset1.stdcol.num <- ncol(subset1.NoFlanks)-2
+    new.subset1 <- subset1.NoFlanks[,c(1, subset1.meancol.num, subset1.stdcol.num, subset1.stdcol.num+1, subset1.stdcol.num+2)]
+    names(new.subset1) <- c("feature", 
+                            "mean",
+                            "std",
+                            "coord",
+                            "Conditions")
     
     ### file 2
     subset2 <- input2[grep(feature, input2$V1),]
@@ -84,6 +93,14 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
     subset2.NoFlanks$coordinates <- 1:nrow(subset2.NoFlanks)   # Make a new column with coordinates starting from 1
     subset2.NoFlanks$Conditions <- condition2  
     subset2.mean <- mean(subset2.NoFlanks[,ncol(subset2.NoFlanks)-3])
+    subset2.meancol.num <- ncol(subset2.NoFlanks)-3
+    subset2.stdcol.num <- ncol(subset2.NoFlanks)-2
+    new.subset2 <- subset2.NoFlanks[,c(1, subset2.meancol.num, subset2.stdcol.num, subset2.stdcol.num+1, subset2.stdcol.num+2)]
+    names(new.subset2) <- c("feature", 
+                            "mean",
+                            "std",
+                            "coord",
+                            "Conditions")
     
     #geneName <- feature
     ### get sno/miRNA gene names rather than IDs
@@ -97,12 +114,12 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
     }
     
     ### combine dataframes
-    new.df <- rbind(subset1.NoFlanks, subset2.NoFlanks)
+    new.df2 <- rbind(new.subset1, new.subset2)
     
     ### Plot only if the mean is above the min (arg4)
     if(subset1.mean > mean.cutoff | subset2.mean > mean.cutoff){
-    p = ggplot(new.df, aes(x=coordinates, y=V10, fill=Conditions), show.legend = FALSE) +
-        geom_ribbon(aes(ymin=V10-V11, ymax=V10+V11, colour=Conditions),
+    p = ggplot(new.df2, aes(x=coord, y=mean, fill=Conditions), show.legend = FALSE) +
+        geom_ribbon(aes(ymin=mean-std, ymax=mean+std, colour=Conditions),
                     alpha=0.3) +
         geom_line(size=0.8) +
         xlab('Nucleotide position') +
