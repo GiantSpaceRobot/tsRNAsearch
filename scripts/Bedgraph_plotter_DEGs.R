79cb90a889d32527e28032e3ee21acc9e54e8e0b
Paul Donovan <pauldonovandonegal@gmail.com>
2019-04-29 14:52:11 +0100

    Updated Bedgraph_plotter_DEGs.R to suppress NULL messages
    Former-commit-id: fb01933c67860d009013d431d8dac64adc3c1fee

--- scripts/Bedgraph_plotter_DEGs.R
+++ scripts/Bedgraph_plotter_DEGs.R
@@ -11,7 +11,6 @@ args = commandArgs(trailingOnly=TRUE)
 
 ### Change this if you want only to plot features with a higher mean coverage
 ### (e.g. mean coverage of 100 reads is 'mean.cutoff <- 100')
-#mean.cutoff <- 0
 mean.cutoff <- as.integer(args[5])
 
 ### Check if the correct number of command line arguments were provided. If not, return an error.
@@ -23,32 +22,18 @@ if (length(args)==0) {
 library(ggplot2)
 
 ### Read in files and get file basename
-#input1 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/Intermediate-files/condition1_concatenated_mean_stdev.tiRNA.depth")
-#input2 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/Intermediate-files/condition2_concatenated_mean_stdev.tiRNA.depth")
-#input3 <- read.table("/home/paul/Documents/Pipelines/tsRNAsearch/subset-fake/Results/Data/DE_Results/DESeq2/DEGs_names-only.txt")
-#mean.cutoff = 0
-
 input1 <- read.table(args[1])
 input2 <- read.table(args[2])
 if (file.size(args[3]) == 0) { # Check if features file contains any lines
   message <- paste0("The file ", as.character(args[3]), " was empty. There were no features to plot")
   write.table(x = message, file = paste0(args[3], ".README"))
 } else {
-
   input3 <- read.table(args[3])
-  
-  #if (nrow(input3) < 1) {
-  #  message <- paste0("The file ", args[3], " was empty. There were no features to plot")
-  #  write.table(x = message, file = paste0(args[3], ".txt"))
-  #} else {
-  
   if (length(args)==6) {
     GTF <- read.table(args[6], sep = "\t")
-    #GTF <- read.table("/home/paul/Documents/Pipelines/tirna-pipeline/DBs/hg19-snomiRNA.gtf", sep = "\t")
   } 
   
   ### Get names of conditions from arg3 name for writing in plots
-  #file3 <- "Angio_vs_Vehic_Features_DiffExprtiRNAs.pdf"
   file3 <- basename(args[4])
   conditions.for.plots <- strsplit(x = file3, "_Features")[[1]][1] #Must do double index to access the resulting list from strsplit
   condition1 <- strsplit(x = conditions.for.plots, "_vs_")[[1]][1]
@@ -68,10 +53,7 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
     } else next  # Skip this iteration and do not generate plot
     
     ### file 1
-    #feature <- "ValCAC"
     subset1 <- input1[grep(feature, input1$V1),]
-    #subset1.NoFlanks <- tail(subset1, -10)
-    #subset1.NoFlanks <- head(subset1.NoFlanks, -10)
     subset1.NoFlanks <- subset1
     subset1.NoFlanks$coordinates <- 1:nrow(subset1.NoFlanks)   # Make a new column with coordinates starting from 1
     subset1.NoFlanks$Conditions <- condition1
@@ -87,8 +69,6 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
     
     ### file 2
     subset2 <- input2[grep(feature, input2$V1),]
-    #subset2.NoFlanks <- tail(subset2, -10)
-    #subset2.NoFlanks <- head(subset2.NoFlanks, -10)
     subset2.NoFlanks <- subset2
     subset2.NoFlanks$coordinates <- 1:nrow(subset2.NoFlanks)   # Make a new column with coordinates starting from 1
     subset2.NoFlanks$Conditions <- condition2  
@@ -102,7 +82,6 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
                             "coord",
                             "Conditions")
     
-    #geneName <- feature
     ### get sno/miRNA gene names rather than IDs
     if (length(args)==6) {
       featureRows <- GTF[grep(feature, GTF$V9),]
@@ -132,10 +111,9 @@ if (file.size(args[3]) == 0) { # Check if features file contains any lines
     }
   }
   
-  #pdf("/home/paul/Documents/Pipelines/tirna-pipeline/Output2.pdf")
   pdf(args[4])
   for(feature in featuresUnion) {
-    print(plot_list[[feature]])
+    capture.output(print(plot_list[[feature]])) # Capture.output suppresses print output 'NULL'
   }
-  garbage <- dev.off() # Suppress message 'NULL'
+  invisible(dev.off()) # Suppress message 'NULL'
 }
