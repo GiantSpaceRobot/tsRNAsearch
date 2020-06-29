03e51dad3da582bb0e88841beaf54db269cc55d6
Paul Donovan <pauldonovandonegal@gmail.com>
2020-06-29 13:58:57 +0100

    Updating conda installation steps

--- setup_conda.sh
+++ setup_conda.sh
@@ -60,7 +60,16 @@ function rat_setup () {
 }
 
 ### Set up conda environment
-conda info # Check if conda installed
+# Check if conda installed:
+if [[ $(conda info | wc -l) -ge 1 ]];
+then
+	echo "Conda is installed"
+else
+	echo "Conda is not installed. Please install conda (Miniconda2 Linux 64 bit Python2.7) using the following guide: https://docs.conda.io/en/latest/miniconda.html#linux-installer"
+	exit 1
+fi
+
+### Create conda environment
 conda create -y --name tsrnasearch_env python=2.7 # Create new environment with python 2.7
 source activate tsrnasearch_env # Activate new environment
 
